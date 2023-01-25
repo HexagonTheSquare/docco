a04593483bc843cd5a65de1e0e09510421b79c67
Hexagon <hexthesquare@gmail.com>
2023-01-25 01:43:57 +0100

    Arduino functions done and ready for practical testing, slave board non-functional.

--- master/master.ino
+++ master/master.ino
@@ -17,21 +17,15 @@ SERIAL PORT: COM?
 #define DEFAULT_BAUD 9600
 #define SLAVE_ADRESS 9
 
-// constants
-const String fingers[5] = {
-  "thumb",
-  "index",
-  "middle",
-  "ring",
-  "pinky"      
-};
-
 // structs
 struct FingerServo {
-  int potpin;
   int pos;
   Servo servo;
-};
+} thumb1, thumb2, thumb3,
+  index1, index2, index3, 
+  middle1, middle2, middle3, 
+  ring1, ring2, ring3, 
+  pinky1, pinky2, pinky3;
 
 struct SlaveCommand {
   String action;
@@ -68,11 +62,28 @@ void loop() {
     switch(dataType) {
       case 'P':
       // SET FINGER POSITION
+      thumb1.pos = values[0]; thumb2.pos = values[0]; thumb3.pos = values[0];
+      index1.pos = values[1]; index2.pos = values[1]; index3.pos = values[1];
+      middle1.pos = values[2]; middle2.pos = values[2]; middle3.pos = values[2];
+      ring1.pos = values[3]; ring2.pos = values[3]; ring3.pos = values[3];
+      pinky1.pos = values[4]; pinky2.pos = values[4]; pinky3.pos = values[4];
       break;
       case 'S':
       // SET SERVO PINS
+      thumb1.servo.attach(values[0]); thumb2.servo.attach(values[1]); thumb3.servo.attach(values[2]);
+      index1.servo.attach(values[3]); index2.servo.attach(values[4]); index3.servo.attach(values[5]);
+      middle1.servo.attach(values[6]); middle2.servo.attach(values[7]); middle3.servo.attach(values[8]);
+      ring1.servo.attach(values[9]); ring2.servo.attach(values[10]); ring3.servo.attach(values[11]);
+      pinky1.servo.attach(values[12]); pinky2.servo.attach(values[13]); pinky3.servo.attach(values[14]);            
       break;
     }    
   }
-
+  
+  
+  Serial.print(thumb1.pos); Serial.print(":"); Serial.print(thumb2.pos); Serial.print(":"); Serial.print(thumb3.pos); Serial.print(" ");
+  Serial.print(index1.pos); Serial.print(":"); Serial.print(index2.pos); Serial.print(":"); Serial.print(index3.pos); Serial.print(" ");
+  Serial.print(middle1.pos); Serial.print(":"); Serial.print(middle2.pos); Serial.print(":"); Serial.print(middle3.pos); Serial.print(" ");
+  Serial.print(ring1.pos); Serial.print(":"); Serial.print(ring2.pos); Serial.print(":"); Serial.print(ring3.pos); Serial.print(" ");
+  Serial.print(pinky1.pos); Serial.print(":"); Serial.print(pinky2.pos); Serial.print(":"); Serial.print(pinky3.pos); Serial.print(" ");
+    
 }
